1200b212a70e04adb2f27d725595fc33e43d7037
Hillsdej <hillsdej@uni.coventry.ac.uk>
2017-02-28 14:01:33 +0000

    Update buffer.c

--- buffer.c
+++ buffer.c
@@ -12,26 +12,31 @@ int main(int argc, char *argv[])
     int rd_size = 1, wr_size;
     char buf[BUF_SIZE];   /*Declares buffer size*/
 
-    if (argc != 3)
+    if (argc != 3) /*if there aren't 3 arguments, the program will exit*/
     {
+        /*print statement goes here*/
         exit(1);
     }
 
-    in_fd = open(argv[1], O_RDONLY); /*Get input file name*/
-    if (in_fd < 0)
+    in_fd = open(argv[1], O_RDONLY); /*Get input file name. in_fd stores num related to file*/
+    if (in_fd < 0) /*gets negative number if file is not there*/
     {
+        /*another print statement,saying file not found*/
         exit(2);
     }
 
     out_fd = creat(argv[2],OUTPUT_MODE); /*create output file*/
-    if (out_fd < 0)
+    if (out_fd < 0) /*checks if file is created*/
     {
+        /*print statement which says file not created*/
         exit(3);
     }
     
     while (rd_size > 0)
     {
         rd_size = read(in_fd, buf,BUF_SIZE); /*repeatedly read from original file into buffer*/
+        /*can use this to read the number of characters*/
+        /*for loop to check each character*/
         if (rd_size <0)
         {
             exit(4);
@@ -46,3 +51,7 @@ int main(int argc, char *argv[])
         }
     }
 }
+
+/*count number of times the while loop goes around to count the number of times the buffer is filled, remember to subtract one*/
+/*declare another buffer, to compare the two documents. declare other numbers to store these. compare character to character "easy way"*/
+
