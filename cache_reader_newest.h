a8b95f4c8ca794f33ab55ba1a3df36f1bef05545
Hillsdej <hillsdej@uni.coventry.ac.uk>
2017-03-03 18:38:32 +0000

    Update cache_reader_newest.h

--- cache_reader_newest.h
+++ cache_reader_newest.h
@@ -1,13 +1,11 @@
-#define _GNU_SOURCE
+#define _GNU_SOURCE //newly added
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <fcntl.h> //newly added includes
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <malloc.h>
-
-
 
 /*sys.types are probs needed*/
 /*find out what includes need to be present for the system calls for open,read and close*/ 
@@ -22,20 +20,14 @@ typedef struct{
                      //  same length as "bufferlength"
 } cr_file;
 
-
-
-
 //Open a file with a given size of buffer to cache with
 cr_file* cr_open(char* filename, int buffersize);
-
 
 //Close an open file
 void cr_close(cr_file* f);
 
 //Read a byte.  Will return EOF if empty.
 char cr_read_byte(cr_file* f);
-
-
 
 //---------------------------------------------------------
 
