3230d5f728f2008f7020852519285d6a4f2ea88b
Hillsdej <hillsdej@uni.coventry.ac.uk>
2017-04-10 19:51:53 +0100

    Update prodcon_example2.c

--- prodcon_example2.c
+++ prodcon_example2.c
@@ -9,36 +9,40 @@ int bufferlength=8; //Limited buffer length
 //what could we do about this?
 
 int main(int argc, char argv[]){
-  int producedpos=bufferlength+1;
-  int consumedpos=bufferlength+2;
+  
+  int prodposition=bufferlength+1;
+  int conposition=bufferlength+2;
+  
   //Create shared memory segment
   int shm_id=shmget(ftok("prodcon_example2.c",2),bufferlength, 
 		    0666|IPC_CREAT);
 
-  //Use our source file as the "key"
+  //Use source file as the "key"
   int id=se207_semget("prodcon_example2.c",0);
 
   char* data; //For our pointer to shared memory...
 
   int pid=fork();  
   if(pid){
+    
     //P1 - CONSUMER
     shm_id=shmget(ftok("prodcon_example2.c",2),0,006);
 
     //Attach the shared buffer
     data = shmat(shm_id, (void *)0, 0);
-    data[producedpos] = 0;
-    data[consumedpos] = 0;
+    data[prodposition] = 0;
+    data[conposition] = 0;
+    
     while(1){
     
       se207_wait(id);
-      while (data[producedpos] == data[consumedpos]);
-      printf("Consuming item number %d...\n",data[consumedpos]);
+      while (data[prodposition] == data[conposition]);
+      printf("Consuming item number %d...\n",data[conposition]);
       sleep(10);
-      char item=data[data[consumedpos]];
+      char item=data[data[conposition]];
       
-      printf("Consumed item number %d.  Item value was %d\n",data[consumedpos],item);
-      data[consumedpos] = (data[consumedpos] + 1) % bufferlength;
+      printf("Consumed item number %d.  Item value was %d\n",data[conposition],item);
+      data[conposition] = (data[conposition] + 1) % bufferlength;
     }
 
     //Detatch
@@ -57,15 +61,16 @@ int main(int argc, char argv[]){
     shm_id=shmget(ftok("prodcon_example2.c",2),0,006);                   
     //Attach the shared buffer
     data = shmat(shm_id, (void *)0, 0);
-    data[producedpos] = 0;
-    data[consumedpos] = 0;
+    data[prodposition] = 0;
+    data[conposition] = 0;
     while(1){
-    while((data[producedpos] + 1) % bufferlength == data[consumedpos]);
-      printf("Producing item number %d...\n",data[producedpos]);
+    
+    while((data[prodposition] + 1) % bufferlength == data[conposition]);
+      printf("Producing item number %d...\n",data[prodposition]);
       sleep(2);
-      data[data[producedpos]]=data[producedpos]*2; //Simple data, easy to check.
-      printf("Produced item number %d.  Value is %d\n",data[producedpos],data[data[producedpos]]);
-           data[producedpos] = (data[producedpos] +1) % bufferlength;
+      data[data[prodposition]]=data[prodposition]*2; //Simple data, easy to check.
+      printf("Produced item number %d.  Value is %d\n",data[prodposition],data[data[prodposition]]);
+           data[prodposition] = (data[prodposition] +1) % bufferlength;
       se207_signal(id);
     }
     //Detatch
